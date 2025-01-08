756039701a91ce3dd157789fb77fec4f825eeca6
Ruben <rubencidep@gmail.com>
2025-01-08 23:56:36 +0100

    Second batch of exercises: 1 to 9 solved

--- imagenCS2/imagenes.py
+++ imagenCS2/imagenes.py
@@ -158,3 +158,123 @@ def box_color_inverter(ruta,coordX,coordY):
     cv2.imshow("Imagen con colores invertidos", imagen_resized)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+###########################################################################################
+    # Ejercicio 6
+    # Crea una función que pasándole la ruta de una imagen, la recorte para evitar dimensiones
+    # con valores impares
+###########################################################################################
+
+def resize_image(ruta):
+    imagen = cv2.imread(ruta,cv2.IMREAD_UNCHANGED)
+    alto, ancho, canales = imagen.shape
+    print(f"Alto: {alto} píxeles")
+    print(f"Ancho: {ancho} píxeles")
+
+    if alto %2 !=0:
+        alto -= 1
+    if ancho % 2 != 0:
+        ancho -= 1
+    print('Imagen redimensionada')
+    print(f"Alto: {alto} píxeles")
+    print(f"Ancho: {ancho} píxeles")
+
+    # Redimensionar la imagen para que sus dimensiones sean pares
+    image_resized = imagen[0:alto, 0:ancho]
+
+    # Usar os.path.splitext() para separar el nombre y la extensión
+    nombre, extension = os.path.splitext(ruta)
+    # Crear la nueva ruta con el nombre modificado y la misma extensión
+    nueva_imagen = nombre + '_resized' + extension
+    # La guardamos en memoria en un fichero distinto y devolvemos la ruta
+    cv2.imwrite(nueva_imagen, image_resized)
+
+
+###########################################################################################
+# Ejercicio 7
+# Crea una función que pasándole la ruta de una imagen, retorne la imagen espejada
+###########################################################################################
+
+def mirror_image(ruta):
+    imagen = cv2.imread(ruta,cv2.IMREAD_UNCHANGED)
+
+    mirror_image = cv2.flip(imagen, 1)
+
+    imagen_resized = cv2.resize(imagen, (400, 300))
+    imagen_resized_mirror = cv2.resize(mirror_image, (400, 300))
+
+    cv2.imshow('Imagen Original', imagen_resized)
+    cv2.imshow('Imagen Espejo', imagen_resized_mirror)
+
+    # Usar os.path.splitext() para separar el nombre y la extensión
+    nombre, extension = os.path.splitext(ruta)
+    # Crear la nueva ruta con el nombre modificado y la misma extensión
+    nueva_imagen = nombre + '_mirror' + extension
+    # La guardamos en memoria en un fichero distinto y devolvemos la ruta
+    cv2.imwrite(nueva_imagen, mirror_image)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+###########################################################################################
+# Ejercicio 8
+# Crea una función que pasándole la ruta de una imagen, invierta la mitad izquierda y la
+# copie en la derecha.
+###########################################################################################
+
+def horizontal_half_mirror_image(ruta):
+    # Cargar la imagen
+    imagen = cv2.imread(ruta, cv2.IMREAD_UNCHANGED)
+    # Obtener las dimensiones de la imagen original
+    alto, ancho, canales = imagen.shape
+
+    imagen[:, ancho // 2:] = cv2.flip(imagen[:, :ancho // 2], 1)
+
+    cv2.imshow('Imagen mitad espejo', imagen)
+
+    # Usar os.path.splitext() para separar el nombre y la extensión
+    nombre, extension = os.path.splitext(ruta)
+    # Crear la nueva ruta con el nombre modificado y la misma extensión
+    nueva_imagen = nombre + '_halfmirrorH' + extension
+    # La guardamos en memoria en un fichero distinto y devolvemos la ruta
+    cv2.imwrite(nueva_imagen, imagen)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+###########################################################################################
+# Ejercicio 9
+# Crea una función que pasándole la ruta de una imagen, invierta la mitad superior y la copie
+#  en la inferior, efecto espejo por la horizontal.
+###########################################################################################
+
+def vertical_half_mirror_image(ruta):
+    # Cargar la imagen
+    imagen = cv2.imread(ruta, cv2.IMREAD_UNCHANGED)
+    # Obtener las dimensiones de la imagen original
+    alto, ancho, canales = imagen.shape
+
+    imagen[ alto // 2:,:] = cv2.flip(imagen[:alto // 2,:], 0)
+
+    #cv2.imshow('Imagen mitad espejo', imagen)
+
+    # Usar os.path.splitext() para separar el nombre y la extensión
+    nombre, extension = os.path.splitext(ruta)
+    # Crear la nueva ruta con el nombre modificado y la misma extensión
+    nueva_imagen = nombre + '_halfmirrorV' + extension
+    # La guardamos en memoria en un fichero distinto y devolvemos la ruta
+    cv2.imwrite(nueva_imagen, imagen)
+
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
+    return imagen
+
+###########################################################################################
+# Ejercicio 10
+#  Crea una función que pasándole la ruta de una imagen, genere un documento html donde
+#  muestre la imagen original y las generadas en las tres funciones anteriores en una tabla
+###########################################################################################
+
+def generate_html(ruta):
+    return
